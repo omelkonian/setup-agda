2964d61116f5ca8e5757d5f69c90478d8f7a30cd
Orestis Melkonian <melkon.or@gmail.com>
2020-11-14 21:57:53 +0200

    Use setup-haskell as a pre-setup script

--- src/setup-agda.ts
+++ src/setup-agda.ts
@@ -1,42 +1,24 @@
 import * as core from '@actions/core';
 import * as io from '@actions/io';
-// import {readFileSync} from 'fs';
-// import {join} from 'path';
-// import * as vm from 'vm';
-import * as hso from 'setup-haskell/src/opts';
-import * as hsi from 'setup-haskell/src/installer';
 import {getOpts} from './opts';
 import type {Options} from './opts';
 import {exec} from '@actions/exec';
 
 (async () => {
   try {
-    core.info('Preparing to setup a Agda environment');
-    // Setup Haskell with stack enabled
-    // const hsopts: hso.Options = {
-    //   ghc: {enable: true, raw: '8.6.5', resolved: '8.6.5'},
-    //   cabal: {enable: true, raw: 'latest', resolved: 'latest'},
-    //   stack: {raw: 'latest', resolved: 'latest', enable: true, setup: true}
-    // };
-    await hsi.installTool(
-      'stack' as hso.Tool,
-      'latest',
-      process.platform as hso.OS
+    core.info(
+      'Preparing to setup a Agda environment (after Haskell has been setup)'
     );
-    await exec('stack', ['setup', '8.6.5']);
-    // vm.runInNewContext(
-    //   readFileSync(join(__dirname, '..', 'dist', 'setup-haskell.js'), 'utf8'),
-    //   {'ghc-version': '8.6.5', 'enable-stack': true, 'stack-version': 'latest'}
-    // );
-
-    core.info('Preparing to setup a Agda environment');
-    const opts: Options = getOpts();
     const home = `${process.env.HOME}`;
+    core.info(`HOME: ${home}`);
     const cur = `${process.env.GITHUB_WORKSPACE}`;
+    core.info(`GITHUB_WORKSPACE: ${cur}`);
+    const opts: Options = getOpts();
+    core.info(`Options are: ${JSON.stringify(opts)}`);
 
     // Install Agda and its standard library
     core.addPath(`${home}/.local/bin/`);
-    io.mkdirP(`${home}/.agda`);
+    await io.mkdirP(`${home}/.agda`);
     await exec(
       `${cur}/scripts/install-agda.sh ${home} ${opts.agda} ${opts.stdlib}`
     );
